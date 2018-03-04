c8a49a6ca9efd686b532aa171ef6836df089f90c
mariannyfidelis <mariannyfidelis@gmail.com>
2018-03-04 00:16:53 -0300

    Primeiro commit - trabalhando XML com Xstream

---
+++ src/Produto.java
@@ -0,0 +1,17 @@
+
+public class Produto {
+
+	public String nome;
+	public double preco;
+	public String descricao;
+	public String categoria;
+	
+	public Produto(String nome, double preco, String descricao, String categoria) {
+		this.nome = nome;
+		this.preco = preco;
+		this.descricao = descricao;
+		this.categoria = categoria;
+	}
+	
+	
+}
