ff7a0876bf1d2740e5387630a9c6e9224d85a4df
mariannyfidelis <mariannyfidelis@gmail.com>
2018-03-05 10:10:28 -0300

    adicionando classe livro para testar herança

---
+++ src/br/com/xml/Livro.java
@@ -0,0 +1,9 @@
+package br.com.xml;
+
+public class Livro extends Produto {
+
+	public Livro(String nome, double preco, String descricao, String categoria, int codigo) {
+		super(nome, preco, descricao, categoria, codigo);
+	}
+
+}
