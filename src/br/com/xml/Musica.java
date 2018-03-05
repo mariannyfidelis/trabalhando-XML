480371739650a8ee9b38622f80bad3dc6e37690b
mariannyfidelis <mariannyfidelis@gmail.com>
2018-03-05 10:11:00 -0300

    adicionando classe Musica para testar serialização no XStream e com herança

---
+++ src/br/com/xml/Musica.java
@@ -0,0 +1,9 @@
+package br.com.xml;
+
+public class Musica extends Produto {
+
+	public Musica(String nome, double preco, String descricao, String categoria, int codigo) {
+		super(nome, preco, descricao, categoria, codigo);
+	}
+
+}
